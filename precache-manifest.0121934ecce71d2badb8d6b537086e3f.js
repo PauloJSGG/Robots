7ae58e1a946588f3b945eb14b2910932aacaffb4
PauloJSGG <paulojsgg@gmail.com>
2019-01-21 22:19:07 +0000

    Updates

---
+++ precache-manifest.0121934ecce71d2badb8d6b537086e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/build/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5ae405a323b1f7dc37b7",
+    "url": "/Robots/build/static/js/runtime~main.5ae405a3.js"
+  },
+  {
+    "revision": "302f4b7f2ecb72d68dd2",
+    "url": "/Robots/build/static/js/main.302f4b7f.chunk.js"
+  },
+  {
+    "revision": "448a22f4366e715fd5f2",
+    "url": "/Robots/build/static/js/1.448a22f4.chunk.js"
+  },
+  {
+    "revision": "302f4b7f2ecb72d68dd2",
+    "url": "/Robots/build/static/css/main.d5a77e29.chunk.css"
+  },
+  {
+    "revision": "448a22f4366e715fd5f2",
+    "url": "/Robots/build/static/css/1.54da82bb.chunk.css"
+  },
+  {
+    "revision": "58a6163c94c1a5036843bc1e0346fc44",
+    "url": "/Robots/build/index.html"
+  }
+];
